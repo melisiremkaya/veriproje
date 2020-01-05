7c020dd21e3675f7b9927515bece3c96324631ca
melisiremkaya <46901446+melisiremkaya@users.noreply.github.com>
2020-01-05 14:03:55 +0300

    Add files via upload

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TXT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(418, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "HTML";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(386, 103);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DOCX";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(76, 205);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(418, 180);
+            this.listBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Bunu mu demek istediniz?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Sonuçlar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Aramak istediğiniz kelimeyi giriniz.";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 400);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Süre : ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(263, 145);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 17);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 452);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,268 @@
+﻿using GemBox.Document;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool dosyadanOku(string aranan, string uzanti)
+        {
+            string yeniyazi = "";
+            string dosya_yolu = @"C:\Users\raman\Desktop\Osman\kelime-listesi" + dosyauzantisi;
+            //Okuma işlem yapacağımız dosyanın yolunu belirtiyoruz.
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Open, FileAccess.Read);
+            //Bir file stream nesnesi oluşturuyoruz. 1.parametre dosya yolunu,
+            //2.parametre dosyanın açılacağını,
+            //3.parametre dosyaya erişimin veri okumak için olacağını gösterir.
+            StreamReader sw = new StreamReader(fs);
+            //Okuma işlemi için bir StreamReader nesnesi oluşturduk.
+            string yazi = sw.ReadLine();
+            while (yazi != null)
+            {
+                yeniyazi += yazi;
+                yazi = sw.ReadLine();
+            }
+            BruteForce(aranan, yeniyazi);
+            //Satır satır okuma işlemini gerçekleştirdik ve ekrana yazdırdık
+            //Son satır okunduktan sonra okuma işlemini bitirdik
+            sw.Close();
+            fs.Close();
+            return true;
+        }
+
+        public void BruteForce(string aranan, string text)
+        {
+            for (int i = 0; i <= text.Length - aranan.Length; i++) //Text ve aranan boyutunun farkı kadar dön
+            {
+                int j = 0;
+                while (j < aranan.Length && aranan[j] == text[i + j]) //Harfler eşitse
+                {
+                    j++;
+                }
+                if (j == aranan.Length) //Karşılaştırma sayısı aranan boyutuna eşitse eşleşme vardır
+                {
+                    //  MessageBox.Show(i.ToString()); // Eşleşme konumunu döndür 
+                    listBox2.Items.Add(i + ". indiste " + aranan + " kelimesi bulunmuştur");
+                }
+            }
+
+        }
+
+        public class Spelling
+        {
+            private Dictionary<String, int> _dictionary = new Dictionary<String, int>();
+            private static Regex _wordRegex = new Regex("[a-z]+", RegexOptions.Compiled);
+
+            public Spelling()
+            {
+                string fileContent = File.ReadAllText(@"C:\Users\raman\Desktop\Osman\big.txt");
+                List<string> wordList = fileContent.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                foreach (var word in wordList)
+                {
+                    string trimmedWord = word.Trim().ToLower();
+                    if (_wordRegex.IsMatch(trimmedWord))
+                    {
+                        if (_dictionary.ContainsKey(trimmedWord))
+                            _dictionary[trimmedWord]++;
+                        else
+                            _dictionary.Add(trimmedWord, 1);
+                    }
+                }
+            }
+
+            public string Correct(string word)
+            {
+                if (string.IsNullOrEmpty(word))
+                    return word;
+
+                word = word.ToLower();
+
+                // known()
+                if (_dictionary.ContainsKey(word))
+                    return word;
+
+                List<String> list = Edits(word);
+                Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+                foreach (string wordVariation in list)
+                {
+                    if (_dictionary.ContainsKey(wordVariation) && !candidates.ContainsKey(wordVariation))
+                        candidates.Add(wordVariation, _dictionary[wordVariation]);
+                }
+
+                if (candidates.Count > 0)
+                    return candidates.OrderByDescending(x => x.Value).First().Key;
+
+                // known_edits2()
+                foreach (string item in list)
+                {
+                    foreach (string wordVariation in Edits(item))
+                    {
+                        if (_dictionary.ContainsKey(wordVariation) && !candidates.ContainsKey(wordVariation))
+                            candidates.Add(wordVariation, _dictionary[wordVariation]);
+                    }
+                }
+
+                return (candidates.Count > 0) ? candidates.OrderByDescending(x => x.Value).First().Key : word;
+            }
+
+            private List<string> Edits(string word)
+            {
+                var splits = new List<Tuple<string, string>>();
+                var transposes = new List<string>();
+                var deletes = new List<string>();
+                var replaces = new List<string>();
+                var inserts = new List<string>();
+
+                // Splits
+                for (int i = 0; i < word.Length; i++)
+                {
+                    var tuple = new Tuple<string, string>(word.Substring(0, i), word.Substring(i));
+                    splits.Add(tuple);
+                }
+
+                // Deletes
+                for (int i = 0; i < splits.Count; i++)
+                {
+                    string a = splits[i].Item1;
+                    string b = splits[i].Item2;
+                    if (!string.IsNullOrEmpty(b))
+                    {
+                        deletes.Add(a + b.Substring(1));
+                    }
+                }
+
+                // Transposes
+                for (int i = 0; i < splits.Count; i++)
+                {
+                    string a = splits[i].Item1;
+                    string b = splits[i].Item2;
+                    if (b.Length > 1)
+                    {
+                        transposes.Add(a + b[1] + b[0] + b.Substring(2));
+                    }
+                }
+
+                // Replaces
+                for (int i = 0; i < splits.Count; i++)
+                {
+                    string a = splits[i].Item1;
+                    string b = splits[i].Item2;
+                    if (!string.IsNullOrEmpty(b))
+                    {
+                        for (char c = 'a'; c <= 'z'; c++)
+                        {
+                            replaces.Add(a + c + b.Substring(1));
+                        }
+                    }
+                }
+
+                // Inserts
+                for (int i = 0; i < splits.Count; i++)
+                {
+                    string a = splits[i].Item1;
+                    string b = splits[i].Item2;
+                    for (char c = 'a'; c <= 'z'; c++)
+                    {
+                        inserts.Add(a + c + b);
+                    }
+                }
+
+                return deletes.Union(transposes).Union(replaces).Union(inserts).ToList();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "";
+            Stopwatch sw = new Stopwatch();
+            listBox2.Items.Clear();
+            dosyauzantisi = ".txt";
+            string aranan = textBox1.Text;
+            sw.Start();
+            dosyadanOku(aranan, dosyauzantisi);
+            sw.Stop();
+            Spelling spelling = new Spelling();
+            string word = "";
+            word = textBox1.Text;
+            if (word != spelling.Correct(word))
+                linkLabel1.Text = spelling.Correct(word);
+            if (listBox2.Items.Count != 0)
+                label4.Text += listBox2.Items.Count + " adet sonuç " + sw.ElapsedMilliseconds.ToString() + " milisaniyede bulunmuştur";
+        }
+        int sayac = 0;
+        public void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+        }
+        string dosyauzantisi = "";
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label4.Text = "";
+            Stopwatch sw = new Stopwatch();
+            listBox2.Items.Clear();
+            dosyauzantisi = ".html";
+            string aranan = textBox1.Text;
+            sw.Start();
+            dosyadanOku(aranan, dosyauzantisi);
+            sw.Stop();
+            Spelling spelling = new Spelling();
+            string word = "";
+            word = textBox1.Text;
+            if (word != spelling.Correct(word))
+                linkLabel1.Text = spelling.Correct(word);
+            if (listBox2.Items.Count != 0)
+                label4.Text += listBox2.Items.Count + " adet sonuç " + sw.ElapsedMilliseconds.ToString() + " milisaniyede bulunmuştur";
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label4.Text = "";
+            Stopwatch sw = new Stopwatch();
+            listBox2.Items.Clear();
+            string aranan = textBox1.Text;
+            string text;
+            dosyauzantisi = ".docx";
+            ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+            var document = new DocumentModel();
+            document = DocumentModel.Load(@"C:\Users\raman\Desktop\Osman\kelime-listesi-turkce.docx");
+            text = document.Sections[0].Blocks.Cast<Paragraph>(0).Inlines.Cast<Run>(0).Text;
+            sw.Start();
+            BruteForce(aranan, text);
+            sw.Stop();
+            Spelling spelling = new Spelling();
+            string word = "";
+            word = textBox1.Text;
+            if (word != spelling.Correct(word))
+                linkLabel1.Text = spelling.Correct(word);
+            if (listBox2.Items.Count != 0)
+                label4.Text += listBox2.Items.Count + " adet sonuç " + sw.ElapsedMilliseconds.ToString() + " milisaniyede bulunmuştur";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            textBox1.Text = linkLabel1.Text;
+            linkLabel1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
